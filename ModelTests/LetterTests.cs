899acf7039ff0a1e6901e6e9ed76fc2ec552e0cb
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-09 14:30:47 -0800

    Pass the first test

---
+++ ModelTests/LetterTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ScrabbleScore.Models;
+using System;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class ScrabbleScoreTests
+  {
+
+    [TestMethod]
+    public void IsScore_ResturnLetter_True()
+    {
+      Score testScore = new Score("A");
+      Assert.AreEqual( typeof(Score), testScore.GetType());
+    }
+
+  }
+}
